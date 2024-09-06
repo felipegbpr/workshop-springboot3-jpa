358f05900beb1e18f0aa609940575ba07feb50dc
Felipe Borges <felipetecacc@gmail.com>
2024-09-05 22:20:02 -0300

    Product

---
+++ src/main/java/com/learning/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.learning.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learning.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
